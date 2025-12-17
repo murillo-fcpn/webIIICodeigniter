c77e5fcbf60f86d149045ec72313a62571a5a59d
Israel Murillo FCPN <imurillof@fcpn.edu.bo>
2025-12-17 08:35:44 -0400

    feat: center de welcome_message

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -1,15 +1,17 @@
 <?= $this->extend('layout/main') ?>
 
 <?= $this->section('content') ?>
-<h2>Sistema de gestion de Construcciones</h2>
-<p>Version 1.0</p>
-<p>Desarrollado por: </p>
-<ul>
-    <li>Eidan Leonel Condori Villca</li>
-    <li>Israel Elias Murillo Flores</li>
-    <li>Diego</li>
-    <li>Valentina</li>
-</ul>
-<p>Fecha: <?= date('Y-m-d') ?> </p>
+<div class="text-center mt-5">
+    <h2>Sistema de gestion de Construcciones</h2>
+    <img src="<?= base_url('assets/img/Logo UMSA.png') ?>" alt="Logo" width="200" class="my-4">
+    <p class="lead">Desarrollado por:</p>
+    <ul class="list-unstyled">
+        <li>Eidan Leonel Condori Villca</li>
+        <li>Israel Elias Murillo Flores</li>
+        <li>Diego</li>
+        <li>Valentina</li>
+    </ul>
+    <p class="mt-4 text-muted">Fecha: <?= date('Y-m-d') ?> </p>
+</div>
 
 <?= $this->endSection() ?>
