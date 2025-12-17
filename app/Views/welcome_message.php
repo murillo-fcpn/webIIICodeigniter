7cd3eb29a853e9febb0de50d36e032df5d85cd2e
Israel Murillo FCPN <imurillof@fcpn.edu.bo>
2025-12-17 08:28:22 -0400

    feat: add  names

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -4,6 +4,12 @@
 <h2>Sistema de gestion de Construcciones</h2>
 <p>Version 1.0</p>
 <p>Desarrollado por: </p>
-<p>Fecha: </p>
+<ul>
+    <li>Eidan Leonel Condori Villca</li>
+    <li>Israel Elias Murillo Flores</li>
+    <li>Diego</li>
+    <li>Valentina</li>
+</ul>
+<p>Fecha: <?= date('Y-m-d') ?> </p>
 
 <?= $this->endSection() ?>
